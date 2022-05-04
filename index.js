8a8b8f2d33c18fdd3b39b7c882aeb5d02415003a
Daniele Sergi <daniele.sergi@plurimedia.it>
2022-05-04 16:15:09 +0200

    updated index

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const plsqlLib= require('./oracle-plsql')
 module.exports = function plsql(app, conf) {
   plsqlLib.init(conf)
 
-  app.get('/rest/plsql/:pkg', plsqlLib.readAll)
-  app.get('/rest/plsql/:pkg/:id', plsqlLib.read)
-  app.post('/rest/plsql/:pkg', plsqlLib.create)
-  app.put('/rest/plsql/:pkg/:id', plsqlLib.update)
-  app.delete('/rest/plsql/:pkg/:id', plsqlLib.delete)
-  app.post('/rest/plsql/pls/:pkg/:method', plsqlLib.method)
+  app.get('/rest/plsql/:pkg', plsqlLib.readAllRoute)
+  app.get('/rest/plsql/:pkg/:id', plsqlLib.readRoute)
+  app.post('/rest/plsql/:pkg', plsqlLib.createRoute)
+  app.put('/rest/plsql/:pkg/:id', plsqlLib.updateRoute)
+  app.delete('/rest/plsql/:pkg/:id', plsqlLib.deleteRoute)
+  app.post('/rest/plsql/pls/:pkg/:method', plsqlLib.methodRoute)
 
   return function (req, res, next) {
     next()
